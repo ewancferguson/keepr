6af127207835012e362af87cac7244de343be1d4
ewaaaaannnn <ewanferg01@gmail.com>
2025-03-11 10:01:51 -0600

    deploy

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:7044' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'dev-2npqq1hq13hwvg56.us.auth0.com'
 export const clientId = 'LcnQKxiv9hmTERKQBe1b2rBIIJCCzcyS'
